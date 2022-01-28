27fe3e61fc43cd78444bb739391689a4dd3df608
Iyonissio <mbryoyo01@gmail.com>
2022-01-28 10:09:33 +0200

    jwt auth

--- autenticacao/jwt.py
+++ autenticacao/jwt.py
@@ -1,6 +1,5 @@
 import jwt
 from rest_framework.authentication import get_authorization_header, BaseAuthentication
-#from authentication.models import User
 from autenticacao.models import User
 from rest_framework import exceptions
 import jwt
@@ -8,14 +7,13 @@ from django.conf import settings
 
 
 class JWTAuthentication(BaseAuthentication):
-
     def authenticate(self, request):
         auth_header = get_authorization_header(request)
         auth_data = auth_header.decode('utf-8')
         auth_token = auth_data.split(" ")
 
         if len(auth_token) != 2:
-            raise exceptions.AuthenticationFailed('Token not valid')
+            raise exceptions.AuthenticationFailed('Token nao valido')
         token = auth_token[1]
 
         try:
